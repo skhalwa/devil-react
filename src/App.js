3f0d96e5b2a92df24a83a422490a5e4de4a41f71
skhalwa <130917606+skhalwa@users.noreply.github.com>
2024-11-15 23:11:02 +0530

    Updated app.js with sahni ji

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <h1>I am lucifer the devil</h1>
+          <h1>Sahni ji tussi gussa km kia kro</h1>
         </p>
         <a
           className="App-link"
