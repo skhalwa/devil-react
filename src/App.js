a733cb1e5773083b38c35f5b5b81b185b3cc780f
skhalwa <130917606+skhalwa@users.noreply.github.com>
2024-11-15 23:28:17 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <h1>Sahni ji tussi gussa km kia kro</h1>
+          <h1>Laal gulab jamun bade majedaar</h1>
         </p>
         <a
           className="App-link"
