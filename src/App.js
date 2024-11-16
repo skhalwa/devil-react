525d3c9e4b7e6a3f860fe9c7e81232885a5103be
skhalwa <130917606+skhalwa@users.noreply.github.com>
2024-11-16 13:06:50 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <h1>Hello I am shubham</h1>
+          <h1>Hello detectice I am lucifer</h1>
         </p>
         <a
           className="App-link"
